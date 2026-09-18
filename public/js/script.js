2dc226ece3f8562508c14844bea840c0bad017a5
Icon Stone Mexico <ubymarkdev@gmail.com>
2026-09-18 17:24:43 -0600

    V2

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,27 @@ boton_contacto.addEventListener("click", () => {
     mensaje.textContent = "Gracias!";
 });
 
+
+const grupo1 = document.querySelectorAll(".grupo-1");
+const grupo2 = document.querySelectorAll(".grupo-2");
+const grupo3 = document.querySelectorAll(".grupo-3");
+
+window.addEventListener("scroll", () => {
+
+    grupo1.forEach((burbuja) => {
+        burbuja.style.transform =
+            `translateY(${window.scrollY * 0.2}px)`;
+    });
+
+    grupo2.forEach((burbuja) => {
+        burbuja.style.transform =
+            `translateY(${window.scrollY * 0.4}px)`;
+    });
+
+    grupo3.forEach((burbuja) => {
+        burbuja.style.transform =
+            `translateY(${window.scrollY * 0.7}px)`;
+    });
+
+});
+
